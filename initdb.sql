9e120b68ef7fabd30c40e44e64f2f8e618900b21
larasodjati <larasatisodjati@gmail.com>
2023-11-09 17:07:07 -0800

    Modify initdb.sql file by adding new tables into it

--- initdb.sql
+++ initdb.sql
@@ -5,10 +5,25 @@ create schema grocery_app;
 \c grocery_app;
 
 
-create table test(
+create table users(
     id serial primary key,
     name varchar(255) not null,
-    birthday date
+    birthday date 
 );
 
-insert into test (name) values ('lara'), ('jack');
+create table shopping_list(
+    id serial primary key,
+    list_name varchar(255) not null,
+    users_id int references users(id)
+);
+
+create table shopping_item(
+    id serial primary key,
+    item_name varchar(255) not null,
+    shopping_list_id int references shopping_list(id)
+);
+
+insert into users (name, birthday) values ('lara', '2000-01-01'), ('jack', '1990-02-02');
+insert into shopping_list (list_name, users_id) values('birthday cake', 1 ), ('healthy salad', 2);
+insert into shopping_item (item_name, shopping_list_id) values ( 'flour', 1), ('sugar', 1), ('egg', 1), ('lettuce', 2), ('tomato', 2), ('kale', 2), ('raisins', 2);
+
