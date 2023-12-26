c21c0fc1a1c71121010eb4d627e336876fbf406e
larasodjati <larasatisodjati@gmail.com>
2023-12-26 13:02:29 -0800

    Set up connection between the db and express app

---
+++ server/src/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: '/home/laravien/Grocery_App/.env' })
+
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: process.env.POSTGRES_USER || process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST || 'localhost',
+    database: process.env.POSTGRES_DB,
+})
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+}
